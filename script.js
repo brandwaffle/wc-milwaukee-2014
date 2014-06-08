eea5087d2884829476c25f7c96256097c6f0a9d8
Pumbaa <deploy-disney@expandtheroom.com>
2014-06-08 08:00:14 +0000

    initial commit with some basic slides

---
+++ script.js
@@ -0,0 +1,39 @@
+(function( $ ) {
+
+	var $animals = $( '#animals' );
+	var $pitbulls = $( '#pitbulls' );
+	
+	function handleClick( e ) {
+		e.preventDefault();
+		
+		var target = e.target;
+		var $target = $( target );
+
+		if( 'duck' === target.ID || 'duck' === $target.parent().attr( 'id' ) ) {
+			alert( 'duck' );
+		} else {
+			alert( 'not a duck' );
+		}
+	}
+	
+	function handlePitbull( e ) {
+		e.preventDefault();
+		
+		var target = e.target;
+		var $target = $( target );
+
+		if( $target.closest( '.human' ).length ) {
+			$target.siblings( '.overlay' ).show().delay(1000).fadeOut( 'slow' );
+		} else {
+			$target.siblings( '.overlay' ).show().delay(1000).fadeOut( 'slow' );
+		}
+	}
+
+	
+	$( document ).ready(function() {
+		$animals.on( 'click', handleClick );
+		$pitbulls.on( 'click', handlePitbull );
+	});
+	
+
+}( jQuery ));
